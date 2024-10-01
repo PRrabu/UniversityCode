9582d0b89b1ffa414ebd6a9648878a4b9dee53d7
Alexandr Oberukhtin UGI-235109 <guglaclass@gmail.com>
2024-10-01 10:24:31 +0500

    ProgramMat.cs

--- ProgramMat.cs
+++ ProgramMat.cs
@@ -4,23 +4,18 @@ internal class RectangleCalculatorV2
 {
     static void Main()
     {
-        // Запрос длины и ширины прямоугольника
         Console.Write("Введите длину прямоугольника: ");
         double length = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Введите ширину прямоугольника: ");
         double width = Convert.ToDouble(Console.ReadLine());
-
-        // Вычисление периметра
+        
         double perimeter = 2 * (length + width);
 
-        // Вычисление площади
         double area = length * width;
 
-        // Вычисление диагонали
         double diagonal = Math.Sqrt(length * length + width * width);
 
-        // Вывод результатов
         Console.WriteLine($"Периметр: {perimeter}");
         Console.WriteLine($"Площадь: {area}");
         Console.WriteLine($"Диагональ: {diagonal}");
