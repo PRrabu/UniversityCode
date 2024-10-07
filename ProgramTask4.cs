4d7a1d82aa77e8d7756afed549c269b96b9e932e
Alexandr Oberukhtin UGI-235109 <guglaclass@gmail.com>
2024-10-07 14:55:40 +0500

    Add files via upload

---
+++ ProgramTask4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {Console.Write("Введите число x: ");
+    var x = double.Parse(Console.ReadLine());
+
+    var y = MyFuncion(x);
+    Console.WriteLine("f(x) =" + y);
+
+    Console.ReadKey();
+    }
+    static double MyFuncion(double x){
+        //throw new NotImplementedException();
+        return Math.Sqrt(Math.Pow(x, 2)+ Math.Sqrt(Math.Pow(x,2)+ 1/Math.Pow(x,2)));
+    }
+}
+
+
