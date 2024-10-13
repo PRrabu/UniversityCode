6f5423577355b0b22ec9a36af2aee996723d57e9
Alexandr Oberukhtin UGI-235109 <guglaclass@gmail.com>
2024-10-13 16:40:58 +0500

    Add files via upload

---
+++ ProgramTask5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        double y = F(2, 3, 1) + F(3, 8, 5) +F(5, 6, 1);
+        Console.WriteLine("x = " + Math.Round(y,3));
+
+        Console.ReadKey(); 
+    }
+
+static double F(double a, double b, double c)
+{
+    return Math.Sqrt((c+Math.Pow(Math.Tan(a),2))/b);
+}
+}
