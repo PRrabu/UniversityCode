167dc8b3407b90ed40bb81b90cb1b301ab7642aa
Alexandr Oberukhtin UGI-235109 <guglaclass@gmail.com>
2024-10-20 17:19:46 +0500

    Add files via upload

---
+++ Task6.1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine ("Введите текст на русском языке: ");
+        var text = Console.ReadLine ();
+        Console.WriteLine (Translate(text));
+
+        Console.ReadKey ();
+    }
+
+    static string Translate(string s)
+    {
+         return s.ToUpper()
+        .Replace("А", "A")
+        .Replace("Б", "B")
+        .Replace("В", "V")
+        .Replace("Г", "G")
+        .Replace("Д", "D")
+        .Replace("E", "E")
+        .Replace("Ж", "ZH")
+        .Replace ("З", "Z")
+        .Replace("И", "I")
+        .Replace("Й", "I")
+        .Replace ("K", "K")
+        .Replace ("Л", "L")
+        .Replace("М", "M")
+        .Replace("Н", "N")
+        .Replace("О", "O")
+        .Replace("П", "P")
+        .Replace("Р", "R")
+        .Replace("С", "S")
+        .Replace("Т", "T")
+        .Replace("У", "U")
+        .Replace("Ф", "F")
+        .Replace("Х", "KH")
+        .Replace("Ц", "TS")
+        .Replace("Ч", "CH")
+        .Replace("Ш", "SH")
+        .Replace("Щ", "SHCH")
+        .Replace("Ъ", "IE")
+        .Replace("Ы", "Y")
+        .Replace("Ь", "")
+        .Replace("Ю", "IU")
+        .Replace("Я", "IA");
+    }
+}
---
+++ Task6.2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string word = "кинематограф";
+
+
+        string word1 = word.Remove(0, 8) + word.Substring(7, 1) + word.Substring(4, 2) + word.Substring(2, 1);
+
+    
+        string word2 = word.Substring(4,1) + word.Substring(3, 1) + word.Substring(6, 1) + word.Substring(9, 1) + word.Substring(7, 1);
+
+        Console.WriteLine("Первое слово: " + word1);
+        Console.WriteLine("Второе слово: " + word2);
+    }
+}
