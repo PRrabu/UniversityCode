62b8a0a9dbe575cb67022bfd2c285004fa392d52
Alexandr Oberukhtin UGI-235109 <guglaclass@gmail.com>
2024-12-26 00:26:58 +0500

    Add files via upload

---
+++ ProgramTask11.1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите натуральное число b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int[] sequence = new int[15];
+        
+        for (int n = 0; n < 15; n++)
+        {
+            sequence[n] = PowerOfTwo(n) - b;
+        }
+
+        PrintArray(sequence);
+    }
+    static int PowerOfTwo(int exponent)
+    {
+        int result = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= 2;
+        }
+        return result;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i > 0)
+            {
+                Console.Write("; ");
+            }
+            Console.Write(array[i]);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ ProgramTask11.3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        double average = CalculateAverage(array);
+
+        Console.WriteLine($"Среднее арифметическое элементов массива: {average:F3}");
+    }
+
+    static double CalculateAverage(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+        return (double)sum / array.Length;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i > 0)
+            {
+                Console.Write("; ");
+            }
+            Console.Write(array[i]);
+        }
+        Console.WriteLine();
+    }
+}
