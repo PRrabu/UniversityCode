1a3cc6236942aba000c5985a615cc6ab89b72787
Alexandr Oberukhtin UGI-235109 <guglaclass@gmail.com>
2024-12-26 00:27:47 +0500

    Add files via upload

---
+++ ProgramTask12.1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        if (AreRowsSorted(array, out int rowIndex, out int colIndex))
+        {
+            Console.WriteLine("Строки массива упорядочены по возрастанию.");
+        }
+        else
+        {
+            Console.WriteLine($"Нарушение порядка: строка {rowIndex + 1}, столбец {colIndex}");
+        }
+    }
+
+    static bool AreRowsSorted(int[,] array, out int rowIndex, out int colIndex)
+    {
+        rowIndex = -1;
+        colIndex = -1;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 1; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] < array[i, j - 1])
+                {
+                    rowIndex = i;
+                    colIndex = j;
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ ProgramTask12.2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        int[] sums = CalculateOddElementSums(array);
+
+        Console.WriteLine("Индексы столбцов и значения сумм нечетных элементов:");
+        for (int col = 0; col < sums.Length; col++)
+        {
+            Console.WriteLine($"Столбец {col + 1}: сумма = {sums[col]}");
+        }
+    }
+
+    static int[] CalculateOddElementSums(int[,] array)
+    {
+        int columns = array.GetLength(1);
+        int[] sums = new int[columns];
+
+        for (int col = 0; col < columns; col++)
+        {
+            int sum = 0;
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                if (array[row, col] % 2 != 0)
+                {
+                    sum += array[row, col];
+                }
+            }
+            sums[col] = sum;
+        }
+
+        return sums;
+    }
+}
