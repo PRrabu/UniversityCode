355e86b102b8f27343ad533351afe72416cbd4b4
Alexandr Oberukhtin UGI-235109 <guglaclass@gmail.com>
2024-12-26 17:59:22 +0500

    Update ProgramTask7.2.cs

--- ProgramTask7.2.cs
+++ ProgramTask7.2.cs
@@ -4,7 +4,7 @@ internal class Program
         {            Console.OutputEncoding = System.Text.Encoding.UTF8;
                         Console.WriteLine("Введите координаты точки (x, y) через пробел"); 
                         var input = Console.ReadLine();
-                        var k = input.IndexOf(" "); //индекс пробела в строке
+                        var k = input.IndexOf(" ");
             var x = double.Parse(input.Substring(0, k));             var y = double.Parse(input.Substring(k + 1));
                         if (IsPointInArea(x, y))
                 Console.WriteLine($"Точка ({x}; {y}) лежит в указанной области");            else
